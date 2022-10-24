8e9101c2b5f2a458e9f62b081c4f342827037bef
leo <leo@longblack.co>
2022-10-24 14:11:29 +0900

    secretKey

--- longblack/settings.py
+++ longblack/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os, json
-import sys
+
+from opensearchpy import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,16 +23,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-ROOT_DIR = os.path.dirname(BASE_DIR)
-# secrets.json의 경로
-SECRETS_PATH = os.path.join(ROOT_DIR, 'secrets.json')
-# json파일을 파이썬 객체로 변환
-secrets = json.loads(open(SECRETS_PATH).read())
+secret_file = os.path.join(BASE_DIR, 'secrets.json') # secrets.json 파일 위치를 명시
 
-# json파일은 dict로 변환되므로, .items()를 호출해 나온 key와 value를 사용해
-# settings모듈에 동적으로 할당
-for key, value in secrets.items():
-    setattr(sys.modules[__name__], key, value)
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting, secrets=secrets):
+    """비밀 변수를 가져오거나 명시적 예외를 반환한다."""
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
