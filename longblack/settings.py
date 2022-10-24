68a42f21613674eef02ed51ed7f417d06347e692
leo <leo@longblack.co>
2022-10-24 14:51:37 +0900

    보안이슈

--- longblack/settings.py
+++ longblack/settings.py
@@ -14,8 +14,6 @@ from pathlib import Path
 import os, json
 import sys
 
-from opensearchpy import ImproperlyConfigured
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
